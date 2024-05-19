c9c9f8ccb63ca524d89f98b1f4f90b966a4ac089
AndrewUAV <145654146+AndrewUAV@users.noreply.github.com>
2024-05-19 10:31:38 -0900

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+
+from .forms import RegisterForm, LoginForm, ProfileForm
+
+
+class SignupView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'users/signup.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('quotes:root')  # Перенаправлення на головну сторінку після реєстрації
+        return render(request, 'users/signup.html', {'form': form})
+
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('quotes:root')  # Перенаправлення на головну сторінку після входу
+        return render(request, 'users/login.html', {'form': form})
+
+
+class LogoutView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        logout(request)
+        return redirect('quotes:root')  # Перенаправлення на головну сторінку після виходу
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users:profile')
+
+    profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'profile_form': profile_form})
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
+
+def register(request):
+    return render(request, 'users/register.html')
